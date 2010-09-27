1ec4bdf70a053d76571bf3070b27be9567b68408
trez <tobias.vehkajarvi@gmail.com>
2010-09-27 23:49:42 +0200

    Added some addressing functions

--- CPU/Helpers.hs
+++ CPU/Helpers.hs
@@ -1,9 +1,15 @@
 module CPU.Helpers
     (
+      -- * Bit
       bitBool
+
+      -- * Monadic
+    , (<+>)
+
+      -- * Addresses.
+    , toAddr
     , mkAddr
     , (<#>)
-    , (<+>)
     ) where
 
 import Control.Monad (liftM2)
@@ -14,13 +20,17 @@ import CPU.Types
 bitBool :: Bits a => Int -> Bool -> a -> a
 bitBool n b = if b then (`setBit` n) else (`clearBit` n)
 
--- | Build a 16-bit address from low and high components.
-mkAddr :: Operand -> Operand -> Address
-mkAddr low high = fromIntegral high `shiftL` 8 + fromIntegral low
-
 -- | Monadic adding.
 (<+>) :: (Num a, Monad m) => m a -> m a -> m a
 (<+>) = liftM2 (+)
+
+-- | Transforms an operand into a Address.
+toAddr :: Operand -> Address
+toAddr = fromIntegral
+
+-- | Build a 16-bit address from low and high components.
+mkAddr :: Operand -> Operand -> Address
+mkAddr low high = toAddr high `shiftL` 8 + toAddr low
 
 -- | Builds a 16-bit address.
 (<#>) :: Monad m => m Operand -> m Operand -> m Address
